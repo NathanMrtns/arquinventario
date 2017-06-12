240974b017389fd8abb1464d6219fe3f210658ed
Nathan <nathanmrtns@gmail.com>
2017-06-12 00:53:17 -0300

    changing back end cors, adding routes and states on front-end

--- front-end/public/controllers/controller.js
+++ front-end/public/controllers/controller.js
@@ -1,8 +1,41 @@
-var myApp = angular.module('myApp', []);
+var app = angular.module('app', ['ui.router']);
 
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
+
+    $stateProvider
+    .state('login', {
+        url: "/login",
+        templateUrl: 'templates/login.html',
+        controller:"AppCtrl"
+    })
+    .state("home",{
+        url:"/home",
+        templateUrl : "templates/home.html"
+    })
+});
+
+app.controller('AppCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
     console.log("Hello World from controller");
 
     $scope.message = "Hello World";
+    $scope.email = "";
+    $scope.password = "";
+
+    $scope.submit = function(){
+        console.log($scope.email);
+        console.log($scope.password);
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/login',
+            data: {
+                "email":$scope.email,
+                "password": $scope.password
+            }
+        }).then(function(response){
+            console.log(response);
+            $state.go("home");
+        });
+    }
     
-}]);﻿
+}]);
