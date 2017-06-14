e21a2dfdf56a2013357e916c26d2f9f1f8cdb8bb
Arthur Antunes <arthuragds@gmail.com>
2017-06-14 15:31:50 -0300

    Adding the .html file of the screen which the user may add a new building to the system.

--- front-end/public/controllers/newBuildingController.js
+++ front-end/public/controllers/newBuildingController.js
@@ -2,38 +2,38 @@ var app = angular.module('app', ['ui.router']);
 
 app.controller('AppCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
 
-    $scope.name = "";
-    $scope.date = "";
-    $scope.style = "";
+	$scope.name = "";
+	$scope.date = "";
+	$scope.style = "";
 	$scope.history = "";
-    $scope.description = "";
-    $scope.tipology = "";
-    $scope.photos = "";
+	$scope.description = "";
+	$scope.tipology = "";
+	$scope.photos = "";
 
-    $scope.submit = function(){
-        console.log($scope.name);
-        console.log($scope.date);
-        console.log($scope.style);
-        console.log($scope.history);
-        console.log($scope.description);
-        console.log($scope.tipology);
-        console.log($scope.photos);
-        $http({
-            method: 'POST',
-            url: 'http://localhost:8080/login',
-            data: {
-                "name":$scope.name,
-                "date": $scope.date,
+	$scope.submit = function(){
+		console.log($scope.name);
+		console.log($scope.date);
+		console.log($scope.style);
+		console.log($scope.history);
+		console.log($scope.description);
+		console.log($scope.tipology);
+		console.log($scope.photos);
+		$http({
+			method: 'POST',
+			url: 'http://localhost:8080/login',
+			data: {
+				"name":$scope.name,
+				"date": $scope.date,
 				"style": $scope.style,
 				"history": $scope.history,
-    			"description": $scope.description,
-    			"tipology": $scope.tipology,
-    			"photos": $scope.photos
-            }
-        }).then(function(response){
-            console.log(response);
-            $state.go("home");
-        });
-    }
-    
+				"description": $scope.description,
+				"tipology": $scope.tipology,
+				"photos": $scope.photos
+			}
+		}).then(function(response){
+			console.log(response);
+			$state.go("home");
+		});
+	}
+
 }]);
