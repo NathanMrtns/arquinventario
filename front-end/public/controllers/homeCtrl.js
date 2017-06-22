973212becd9844401941a3d9ce3076f498ddced3
Arthur Antunes <arthuragds@gmail.com>
2017-06-21 21:36:37 -0300

    Adding the controller of the home page.

--- front-end/public/controllers/homeCtrl.js
+++ front-end/public/controllers/homeCtrl.js
@@ -9,22 +9,10 @@ app.controller('homeCtrl', ['$scope', '$http', '$state', function($scope, $http,
 		$http({
 			method: 'GET',
 			url: 'http://localhost:8080/patrimony',
-			/*
-			data: {
-				"name":$scope.name,
-				"date": $scope.date,
-				"style": $scope.style,
-				"history": $scope.history,
-				"description": $scope.description,
-				"tipology": $scope.tipology,
-				"photos": $scope.photos
-			}
-			*/
 		}).then(function(response){
 			$scope.patrimonies = response.data;
 			console.log($scope.patrimonies);
 		});
-
 	}
 
 	getAllPatrimonies();
