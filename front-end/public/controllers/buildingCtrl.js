515ef8eb019bbcb6037d831b40a622417ae45300
Arthur Antunes <arthuragds@gmail.com>
2017-06-22 02:30:19 -0300

    Showing all the information about an specific grammar after selecting it on the home page.

--- front-end/public/controllers/buildingCtrl.js
+++ front-end/public/controllers/buildingCtrl.js
@@ -2,6 +2,13 @@ var app = angular.module('app');
 
 app.controller('buildingCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
 
+	$scope.name = $state.params.name;
+	$scope.year = $state.params.year;
+	$scope.style = $state.params.style;
+	$scope.history = $state.params.history;
+	$scope.description = $state.params.description;
+	$scope.tipology = $state.params.tipology;
+
 	$scope.goToHomePage = function() {
 		$state.go("home");
 	}
