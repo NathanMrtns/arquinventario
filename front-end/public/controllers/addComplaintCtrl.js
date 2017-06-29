c7ab4be45cfdb6f163f080eeb1c241ad07d6c76f
Arthur Silva <arthur.dias@embedded.ufcg.edu.br>
2017-06-29 11:02:51 -0300

    Removing the address field from the user interface.

--- front-end/public/controllers/addComplaintCtrl.js
+++ front-end/public/controllers/addComplaintCtrl.js
@@ -4,31 +4,21 @@ app.controller('addComplaintCtrl', ['$scope', '$http', '$state', function($scope
 
 	$scope.local = "";
 	$scope.date = "";
-	$scope.photos = "";
 	$scope.description = "";
 
 	$scope.submit = function(){
-		data = {
-			"local": $scope.local,
-			"date": $scope.date,
-			"photos": $scope.photos,
-			"description": $scope.description
-		}
-
 		$http({
 			method: 'POST',
 			url: 'http://localhost:8080/report',
 			data: {
 				"local": $scope.local,
 				"date": $scope.date,
-				"photos": $scope.photos,
 				"description": $scope.description
 			}
 		}).then(function(response) {
 			$state.go("home");
 		})
 	}
-
 	$scope.goToHomePage = function() {
 		$state.go("home");
 	}
