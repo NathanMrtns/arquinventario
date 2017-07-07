956dd6c862cf3967ee6afd56f0fb29f8b5e19eb8
Erick Santana Rodrigues <ericksantanardgs01@gmail.com>
2017-07-07 00:44:37 -0300

    adicionando melhorias ao login (wip)

--- front-end/public/controllers/loginController.js
+++ front-end/public/controllers/loginController.js
@@ -1,15 +1,22 @@
 var app = angular.module('app');
 
 app.controller('loginCtrl', ['serverURL', '$scope', '$http', '$state', function(serverURL, $scope, $http, $state) {
+    $scope.viewDiv  = true;
+
     $scope.email    = "";
     $scope.password = "";
 
-    $scope.submit = function(){
+    $scope.senhaRepetida = "";
+    $scope.senha         = "";
+    $scope.nome          = "";
+    $scope.email2        = "";
+
+    $scope.login = function(){
         $http({
             method: 'POST',
             url: serverURL.value+'/login',
             data: {
-                "email":$scope.email,
+                "email" : $scope.email,
                 "password": $scope.password
             }
         }).then(function success(response){
@@ -23,5 +30,26 @@ app.controller('loginCtrl', ['serverURL', '$scope', '$http', '$state', function(
         });
     }
 
+    $scope.signUp = function(){
+        if($scope.senha != $scope.senhaRepetida){
+            $scope.error = "As senhas devem ser as mesmas!";
+        }else{
+            $scope.error = "";
+            console.log($scope.senha + " " + $scope.nome + " " + $scope.email2);
+            $http({
+                method: 'POST',
+                url: serverURL.value+'/user',
+                data: {name: $scope.nome, email: $scope.email2, password: $scope.senha}
+            }).then(function(result){
+                console.log(result);
+                if(result.status == 200){
+                    //localStorage.setItem('token', result.data.token );
+                    $state.go('home');
+                }else{
+                    $scope.error = result.data;
+                }
+            });
+        }
+    }
 }]);
 
