dfed3d20244df93f78119cb1671c6e6a8f24a727
Arthur Antunes <arthuragds@gmail.com>
2017-07-26 22:31:17 -0300

    Saving tickets to the system database.

--- front-end/public/controllers/addTicketCtrl.js
+++ front-end/public/controllers/addTicketCtrl.js
@@ -1,6 +1,25 @@
 var app = angular.module('app');
 
 app.controller('addTicketCtrl', ['serverURL', '$scope', '$http', '$state', function(serverURL, $scope, $http, $state) {
+	$scope.title = "";
+	$scope.address = "";
+	$scope.description = "";
 
+	$scope.submit = function() {
+		$http({
+			method: 'POST',
+			url: serverURL.value + '/ticket',
+			data: {
+				"title": $scope.title,
+				"address": $scope.address,
+				"description": $scope.description
+			}
+		}).then(function(response) {
+			console.log(response);
+		})
+	}
 
+	$scope.goToHomePage = function() {
+		$state.go("home");
+	}
 }]);
