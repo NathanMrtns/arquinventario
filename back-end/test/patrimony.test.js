7c8d050454a5d12cef323fac85b59a3e6ae17c70
Arthur Antunes <arthuragds@gmail.com>
2017-12-09 13:14:00 -0300

    Adding patrimony.test.js to folder in which test files will be stored.

---
+++ back-end/test/patrimony.test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var should = require('should');
+
+var request = require('supertest');
+
+var patrimony = require('../routes/patrimony.js');
+
+var host = 'http://localhost:8080';
+
+describe('Patrimony Tests!', function() {
+    describe('POST/patrimony', function() {});
+
+    describe('GET/patrimony', function() {});
+});
